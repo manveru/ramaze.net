6acb69a508080e0cc6d2d5d68978694056966d45
Michael Fellinger <m.fellinger@gmail.com>
2008-12-05 18:59:13 +0900

    remove last .mkd

--- controller/init.rb
+++ controller/init.rb
@@ -2,10 +2,6 @@ class Controller < Ramaze::Controller
   map nil
   helper :xhtml, :user, :formatting, :aspect, :maruku, :ultraviolet
   engine :Haml
-
-  def mkd(file)
-    maruku(File.read("view/#{file}.mkd"))
-  end
 end
 
 require 'controller/main'
--- controller/main.rb
+++ controller/main.rb
@@ -4,8 +4,4 @@ class MainController < Controller
   before_all do
     @current = Ramaze::Action.current.name
   end
-
-  def learn__getting_started
-    mkd 'learn/getting-started'
-  end
 end
