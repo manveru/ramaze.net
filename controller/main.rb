619f08809c40c91a13a52eb6ca609c63899c2940
Pistos <gitsomegrace.5.pistos@geoshell.com>
2009-01-03 22:27:38 -0500

    Rewrote menu code.

--- controller/main.rb
+++ controller/main.rb
@@ -1,7 +1,19 @@
 class MainController < Controller
+
   layout '/layout/main'
 
   before_all do
     @current = Ramaze::Action.current.name
+    @menu = [
+      { :text => 'Home',      :href => '/',                                },
+      { :text => 'Features',  :href => '/features',                        },
+      { :text => 'Learn',     :href => '/learn',                           },
+      { :text => 'Download',  :href => '/get',                             },
+      { :text => 'Community', :href => '/community',                       },
+      { :text => 'Code',      :href => 'http://github.com/manveru/ramaze', },
+    ]
+    item = @menu.find { |item| item[ :href ] == "/#{@current}" } || @menu[ 0 ]
+    item[ :class ] = 'current'
   end
+
 end
