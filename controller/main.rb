4077fadc1556330d2873662d4311b573330b3a06
Pistos <gitsomegrace.5.pistos@geoshell.com>
2009-01-16 11:39:06 -0500

    Now use RSS of ramazenews Twitter account for main News listing. Keeping older news items hard coded underneath for now.

--- controller/main.rb
+++ controller/main.rb
@@ -18,20 +18,29 @@ class MainController < Controller
 
   private
 
-  def show_feed(desc, link)
+  def show_feed(desc, link, is_twitter = false)
     cache = Ramaze::Cache.feed
 
     if content = cache[link]
       content
     else
-      content = build_feed(link, desc)
+      content = build_feed(link, desc, is_twitter)
       cache.store(link, content, :ttl => 600)
     end
 
     content
   end
 
-  def build_feed(link, desc)
+  def lengthen_url(url)
+    require 'open-uri'
+    require 'json'
+    open( "http://www.longurlplease.com/api/v1.1?q=#{ CGI.escape( url ) }" ) do |http|
+      hash = JSON.parse(http.read)
+      hash.values[0] || url
+    end
+  end
+
+  def build_feed(link, desc, is_twitter = false)
     feed = FeedConvert.parse(open(link))
 
     b = Builder::XmlMarkup.new
@@ -47,8 +56,21 @@ class MainController < Controller
       b.ul do
         feed.items.map do |item|
           b.li do
-            b.span( item.time.strftime( '%Y-%m-%d' ), :class => 'date' )
-            b.a(item.title, :href => item.link)
+            if is_twitter
+              b.span( item.time.strftime( '%Y-%m-%d' ), :class => 'date' )
+              title = item.title.gsub( /^ramazenews: /, '' )
+              title.scan( /\S+/ ) { |word|
+                if word =~ %r{^http://}
+                  url = lengthen_url(word)
+                  b.a( "#{url} ", :href => url )
+                else
+                  b.span( "#{word} " )
+                end
+              }
+            else
+              b.span( item.time.strftime( '%Y-%m-%d' ), :class => 'date' )
+              b.a(item.title, :href => item.link)
+            end
           end
         end
       end
